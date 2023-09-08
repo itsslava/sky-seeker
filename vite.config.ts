07cc19e218ea6a0a819c25b9ea45dd033c45f637
itsslava <77506882+itsslava@users.noreply.github.com>
2023-09-08 17:04:31 +0400

    added babel for IE11 support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import TsconfigPaths from 'vite-plugin-tsconfig-paths';
 import autoprefixer from 'autoprefixer';
+import legacy from 'vite-plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TsconfigPaths()],
+  plugins: [
+    react(),
+    TsconfigPaths(),
+    legacy({
+      targets: ['ie >= 11'],
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
