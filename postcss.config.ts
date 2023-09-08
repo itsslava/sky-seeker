b701079f124a0b756dd6fc18c7eb28b8840e876f
itsslava <77506882+itsslava@users.noreply.github.com>
2023-09-08 16:42:21 +0400

    custom properties support for IE11 added in postcss confif

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,7 +1,9 @@
 import autoprefixer from 'autoprefixer';
+import customProperties from 'postcss-custom-properties';
 
 export default {
   plugins: [
+    customProperties({ preserve: false }),
     autoprefixer({
       overrideBrowserslist: ['last 2 versions', 'IE 11'],
     }),
