270ad59efc1164cdb488df7ad6a92078ab38c6b3
itsslava <77506882+itsslava@users.noreply.github.com>
2023-09-08 15:45:36 +0400

    polyfills, autoprefixer, posttcss config added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import TsconfigPaths from 'vite-plugin-tsconfig-paths';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), TsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['last 2 versions', 'IE 11'],
+        }),
+      ],
+    },
+  },
+});
